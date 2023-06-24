593b8d6aa70cf7432c00e86e1c7a523e78900b62
Abdul Rehman <102576102+BetterLogicTeam@users.noreply.github.com>
2023-06-24 19:12:56 +0500

    connectivity done
    connectivity done on testnet. max button not working

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const BRIDGE_URL = "https://bridge.walletconnect.org";
+// export const RPC_URL = "https://eth-goerli.public.blastapi.io"; goerli test network
+export const RPC_URL = "https://bsc-testnet.publicnode.com";
+export const CHAIN_ID = 97 ;
