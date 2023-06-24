fca9b0ed1912213ce3508e1e833a16bce9674e1e
Abdul Rehman <102576102+BetterLogicTeam@users.noreply.github.com>
2023-06-24 23:32:16 +0500

    connect button issue resolved

--- src/Components/ResponsiveDrawer/ResponsiveDrawer.jsx
+++ src/Components/ResponsiveDrawer/ResponsiveDrawer.jsx
@@ -497,24 +497,32 @@ function ResponsiveDrawer(props) {
                 component="div"
               >
                 <button
-                  class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium css-1y334na"
-                  tabindex="0"
-                  type="button"
-                >
-                  <svg
-                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1etrg7"
-                    focusable="false"
-                    aria-hidden="true"
-                    viewBox="0 0 24 24"
-                    data-testid="WalletIcon"
-                  >
-                    <path d="M18 4H6C3.79 4 2 5.79 2 8v8c0 2.21 1.79 4 4 4h12c2.21 0 4-1.79 4-4V8c0-2.21-1.79-4-4-4zm-1.86 9.77c-.24.2-.57.28-.88.2L4.15 11.25C4.45 10.52 5.16 10 6 10h12c.67 0 1.26.34 1.63.84l-3.49 2.93zM6 6h12c1.1 0 2 .9 2 2v.55c-.59-.34-1.27-.55-2-.55H6c-.73 0-1.41.21-2 .55V8c0-1.1.9-2 2-2z"></path>
-                  </svg>
-                  <p class="MuiTypography-root MuiTypography-body1 css-1qhlbm">
-                    Connect
-                  </p>
-                  <span class="MuiTouchRipple-root css-w0pj6f"></span>
-                </button>
+                class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium css-1y334na"
+                tabindex="0"
+                type="button"
+                onClick={() => showModal()}
+              >
+                {
+                  acc == null ? <>
+                    <svg
+                      class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1etrg7"
+                      focusable="false"
+                      aria-hidden="true"
+                      viewBox="0 0 24 24"
+                      data-testid="WalletIcon"
+                    >
+                      <path d="M18 4H6C3.79 4 2 5.79 2 8v8c0 2.21 1.79 4 4 4h12c2.21 0 4-1.79 4-4V8c0-2.21-1.79-4-4-4zm-1.86 9.77c-.24.2-.57.28-.88.2L4.15 11.25C4.45 10.52 5.16 10 6 10h12c.67 0 1.26.34 1.63.84l-3.49 2.93zM6 6h12c1.1 0 2 .9 2 2v.55c-.59-.34-1.27-.55-2-.55H6c-.73 0-1.41.21-2 .55V8c0-1.1.9-2 2-2z"></path>
+                    </svg>
+                    <p class="MuiTypography-root MuiTypography-body1 css-1qhlbm">
+                      Connect
+                    </p>
+                    <span class="MuiTouchRipple-root css-w0pj6f"></span>
+                  </>
+                    :
+                    "Connected"
+                }
+
+              </button>
               </Typography>
             </Box>
           </div>
